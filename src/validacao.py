3ccecc6a5e96f54a6e5c9efabc9dd069f0bfae87
Miguel Araujo <miguelangeloarauj@gmail.com>
2026-05-10 00:47:36 -0300

    refactor: melhora validações com loop e retorno de valor
    - validar_cpf agora repete o input até CPF ser válido e retorna o valor
    - adiciona validar_atendimento com loop de reentrada
    - adiciona validar_convenio com loop de reentrada

--- src/validacao.py
+++ src/validacao.py
@@ -1,9 +1,22 @@
 from validate_docbr import CPF
+from src.config import TIPOS_ATENDIMENTO, TIPOS_CONVENIO
 
 def validar_cpf(cpf_cliente):
     cpf = CPF()
-    if cpf.validate(cpf_cliente):
-        print('CPF Válido.')
-    else:
-        print('CPF Inválido, tente novamente')
 
+    while cpf.validate(cpf_cliente) == False:
+        print('CPF inválido, tente novamente.')
+        cpf_cliente = input("Digite o CPF do cliente: ")
+    return cpf_cliente
+
+def validar_atendimento(tipo_atendimento):
+    while tipo_atendimento not in TIPOS_ATENDIMENTO:
+        print('Tipo de atendimento inválido, tente novamente.')
+        tipo_atendimento = input("Digite o tipo de atendimento (1 - Consulta, 2 - Exame, 3 - Procedimento): ")
+    return tipo_atendimento
+
+def validar_convenio(tipo_convenio):
+    while tipo_convenio not in TIPOS_CONVENIO:
+        print('Convênio inválido, tente novamente.')
+        tipo_convenio = input("Digite o tipo de convênio (1 - Unimed, 2 - Bradesco Saúde, 3 - SulAmérica, 4 - Amil, 5 - Particular): ")
+    return tipo_convenio
